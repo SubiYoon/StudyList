5bffb424f10c613976e93a71afbc0d85635f37ff
YoonDongSub <yoondongsub92@gmail.com>
2023-05-23 08:41:14 +0900

    WAS 만들어 보기 5일차

--- SelfWAS/src/Main.java
+++ SelfWAS/src/Main.java
@@ -1,10 +1,12 @@
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import javax.print.DocFlavor;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -51,100 +53,166 @@ public class Main {
                 System.out.println("<Header>=====================\r\n" +
                         header +
                         "\r\n</Header>=====================\r\n");
+                if (header.length() > 10) {
+                    // Header 정보 배열로 변경
+                    String[] headerInfo = header.split("\r\n");
+                    // Header정보를 담을 Map
+                    Map<String, String> headerData = new LinkedHashMap<String, String>();
+                    // 첫번째 Row의 정보를 공백을 기준으로 자른 후 Map에 담음
+                    String[] headerFirstRowInfo = headerInfo[0].split(" ");
+                    headerData.put("Method", headerFirstRowInfo[0]);
+                    headerData.put("URL", headerFirstRowInfo[1]);
+                    headerData.put("Protocol", headerFirstRowInfo[2]);
+                    // 두번째 이후의 정보를 ": "을 기준으로 자른 후 Map에 담음
+                    for (int i = 1; i < headerInfo.length; i++) {
+                        if (headerInfo[i].contains(": ")) {
+                            String[] headerRowInfo = headerInfo[i].split(": ");
+                            headerData.put(headerRowInfo[0], headerRowInfo[1]);
+                        }
+                    }
 
-                // Header 정보 배열로 변경
-                String[] headerInfo = header.split("\r\n");
-                // Header정보를 담을 Map
-                Map<String, String> headerData = new LinkedHashMap<String, String>();
-                // 첫번째 Row의 정보를 공백을 기준으로 자른 후 Map에 담음
-                String[] headerFirstRowInfo = headerInfo[0].split(" ");
-                headerData.put("Method", headerFirstRowInfo[0]);
-                headerData.put("URL", headerFirstRowInfo[1]);
-                headerData.put("Protocol", headerFirstRowInfo[2]);
-                // 두번째 이후의 정보를 ": "을 기준으로 자른 후 Map에 담음
-                for (int i = 1; i < headerInfo.length; i++) {
-                    if(headerInfo[i].contains(": ")) {
-                        String[] headerRowInfo = headerInfo[i].split(": ");
-                        headerData.put(headerRowInfo[0], headerRowInfo[1]);
-                    }
-                }
-
-                // Body의 데이터를 담을 Map
-                Map<String, String> bodyData = new LinkedHashMap<String, String>();
-                // Get방식일 경우 Body 정보 담기
-                if (headerData.get("Method").equals("GET") && headerData.get("URL").contains("?")) {
-                    String[] values = headerData.get("URL").replaceFirst("/\\?", "").split("&");
-                    parameterPut(values, bodyData);
-                }
-                // Post방식을 경우 Body 정보 담기
-                if (headerData.get("Method").equals("POST")) {
-                    // header에서 뽑아낸 body(Content의 길이)
-                    int contentLength = 0;
-                    if (headerData.get("Content-Length") != null && !headerData.get("Content-Length").isBlank()) {
-                        contentLength = Integer.parseInt(headerData.get("Content-Length"));
-                    }
-                    byte[] bodyByte = new byte[contentLength];
-                    for (int i = 0; i < contentLength; i++) {
-                        int b = inputStream.read();
-                        bodyByte[i] = (byte) b;
-                    }
-                    // Multipart Related MIME 타입 처리
-                    if(headerData.get("Content-Type").contains("application/x-www-form-urlencoded") || headerData.get("Content-Type").contains("Multipart/related")) {
-                        String body = new String(bodyByte);
-                        String[] values = body.split("&");
-                        // String으로 해석한 데이터를 Map에 넣어줌
+                    // Body의 데이터를 담을 Map
+                    Map<String, String> bodyData = new LinkedHashMap<String, String>();
+                    // Get방식일 경우 Body 정보 담기
+                    if (headerData.get("Method").equals("GET") && headerData.get("URL").contains("?")) {
+                        String[] values = headerData.get("URL").replaceFirst("/\\?", "").split("&");
                         parameterPut(values, bodyData);
-
                     }
+                    // multipart/form-data boundary의 body 체크를 위한 index
+                    final int boundaryBodyCheckPoint;
+                    // Post방식을 경우 Body 정보 담기
+                    if (headerData.get("Method").equals("POST")) {
+                        // header에서 뽑아낸 body(Content의 길이)
+                        int contentLength = 0;
+                        if (headerData.get("Content-Length") != null && !headerData.get("Content-Length").isBlank()) {
+                            contentLength = Integer.parseInt(headerData.get("Content-Length"));
+                        }
+                        byte[] bodyByte = new byte[contentLength];
+                        for (int i = 0; i < contentLength; i++) {
+                            int b = inputStream.read();
+                            bodyByte[i] = (byte) b;
+                        }
+                        // Multipart Related MIME 타입 처리
+                        if (headerData.get("Content-Type").contains("application/x-www-form-urlencoded") || headerData.get("Content-Type").contains("Multipart/related")) {
+                            String body = new String(bodyByte);
+                            String[] values = body.split("&");
+                            // String으로 해석한 데이터를 Map에 넣어줌
+                            parameterPut(values, bodyData);
+                        }
+                        // Multipart/form-data 타입 처리
+                        else if(headerData.get("Content-Type").contains("multipart/form-data")){
+                            for(int i = 0; i<bodyByte.length; i++){
+                                if(bodyByte[i]==13 && bodyByte[i+1]==10 && bodyByte[i+2]==13 && bodyByte[i+3]==10){
+                                    byte[] boundaryHead = Arrays.copyOf(bodyByte, i+4);     // i+5부터는 body
+                                    boundaryBodyCheckPoint = i+5;
+                                    String parseBoundaryHead = new String(boundaryHead);
+                                    System.out.println("=============boundaryHeader=============\r\n" +
+                                            parseBoundaryHead +
+                                            "=============boundaryHeader=============\r\n"
+                                    );
+                                    break;
+                                }
+                            }
+                            //TODO: boundary마지막 부분 위치 잡고 그 boundaryHeader부분 이하부터 데이터 읽을 수 있도록 처리
+                        }
+                    }
+
+                    // Body정보 출력
+                    System.out.println("<Body>===========================\r\n" +
+                            bodyData +
+                            "\r\n</Body>===========================");
+                    // Charset 확인용 print
+                    System.out.println("Charset.defaultCharset(): " + Charset.defaultCharset());
+
+                    OutputStream outputStream = socket.getOutputStream();
+                    String msg = "";
+                    String serverRoot = "/Users/dongsubyoon/Downloads";
+                    if (headerData.get("Method").equals("GET") && headerData.get("URL").contains("serverRoot")) {
+                        File file = new File(serverRoot + headerData.get("URL"));
+                        if (!file.exists()) {
+                            outputStream.write(new String("HTTP/1.1 404 Not Found\r\n").getBytes());
+                            socket.close();
+                            continue;
+                        }
+                        boolean isFile = file.isFile();
+                        if (isFile) {
+                            Path path = Paths.get(serverRoot + headerData.get("URL"));
+                            long fileSize = Files.size(path);
+                            outputStream.write(new String("HTTP/1.1 200 OK\r\n").getBytes());
+                            outputStream.write(new String("Content-Length:" + fileSize + "\r\n").getBytes());
+                            outputStream.write(new String("Content-Type:application/jpeg\r\n\r\n").getBytes());
+
+                            FileInputStream fis = new FileInputStream(serverRoot + headerData.get("URL"));
+                            //TODO: readAllbytes말고 하나씩 읽는거로 실행
+                            byte[] fileBytes = fis.readAllBytes();
+                            outputStream.write(fileBytes);
+                        } else {
+                            // directory
+                            FileSerarch list = new FileSerarch();
+                            String[] folderList = list.searchAllFolderList(headerData.get("URL"));
+                            int cutURL = headerData.get("URL").substring(0, headerData.get("URL").length() - 1).lastIndexOf("/");
+                            msg += "<meta charset='utf-8'>\r\n";
+                            msg += "<link rel='icon' href='data:,'/>\r\n";  //favicon.ico 를 로드하지 않게 설정
+                            msg += "<body>\r\n";
+                            msg += "<h3>마지막에 '*'가 붙어있으면 파일을 다운로드 합니다.</h1>\r\n";
+                            msg += "    <ul>\r\n";
+                            msg += "        <li><a href='" + headerData.get("URL").substring(0, cutURL + 1) + "'>../</a></li>\r\n";
+                            for (int i = 0; i < folderList.length; i++) {
+                                if (folderList[i].substring(folderList[i].length() - 1, folderList[i].length()).equals("*")) {
+                                    msg += "        <li><a href='" + folderList[i].substring(0, folderList[i].length()-1) + "' download>" + folderList[i] + "</a></li>\r\n";
+                                } else if (folderList[i].substring(folderList[i].length() - 1, folderList[i].length()).equals("/")) {
+                                    msg += "        <li><a href='" + folderList[i] + "'>" + folderList[i] + "</a></li>\r\n";
+                                }
+                            }
+                            msg += "    </ul>\r\n";
+                            msg += "    <a href='/'>처음으로 돌아가기</a>";
+                            msg += "</body>\r\n";
+                            outputStream.write(new String("HTTP/1.1 200 OK\r\n").getBytes());
+                            outputStream.write(new String("Content-Length:" + msg.getBytes().length + "\r\n").getBytes());
+                            outputStream.write(new String("Content-Type:text/html,charset=UTF-8\r\n\r\n").getBytes());
+                        }
+
+
+                    } else {
+                        msg += "<meta charset='utf-8'>\r\n";
+                        msg += "<link rel='icon' href='data:,'/>\r\n";  //favicon.ico 를 로드하지 않게 설정
+                        msg += "<script type='text/javascript'>\r\n";
+                        msg += "    function console_print(){\r\n";
+                        msg += "        var id = document.getElementById('Id').value;\r\n";
+                        msg += "        console.log(id);\r\n";
+                        msg += "    };\r\n";
+                        msg += "</script>\r\n";
+                        msg += "<body>\r\n";
+                        msg += "    <input id='Id' type='text'/>\r\n";
+                        msg += "    <a id='click' href='#' onclick='console_print()'>click</a></br></br>\r\n";
+                        msg += "    <form method='POST' action='/'>\r\n";
+                        msg += "        <input type='text' name='sampleIdPost'/>\r\n";
+                        msg += "        <input type='password' name='samplePwdPost'/>\r\n";
+                        msg += "        <button>POST_sumbit</button>\r\n";
+                        msg += "    </form>\r\n";
+                        msg += "    <form method='GET' action='/'>\r\n";
+                        msg += "        <input type='text' name='sampleIdGet'/>\r\n";
+                        msg += "        <input type='text' name='samplePwdGet'/>\r\n";
+                        msg += "        <button>GET_sumbit</button>\r\n";
+                        msg += "    </form>\r\n";
+                        msg += "    <form method='POST' action='/' enctype='multipart/form-data'>\r\n";
+                        msg += "        <input type='file' name='sampleFile'/>\r\n";
+                        msg += "        <button>File_sumbit</button>\r\n";
+                        msg += "    </form>\r\n";
+                        msg += "    <a href='/serverRoot/'>폴더 탐색</a>\r\n";  //TODO: 뒤에 / 안붙이면 왜??
+                        msg += "</body>\r\n";
+                        outputStream.write(new String("HTTP/1.1 200 OK\r\n").getBytes());
+                        outputStream.write(new String("Content-Length:" + msg.getBytes().length + "\r\n").getBytes());
+                        outputStream.write(new String("Content-Type:text/html,charset=UTF-8\r\n\r\n").getBytes());
+                    }
+                    outputStream.write(msg.getBytes());
+
+                    //화면 출력
+                    outputStream.flush();
+                    inputStream.close();
+                    outputStream.close();
+                    socket.close();
                 }
-
-                // Body정보 출력
-                System.out.println("<Body>===========================\r\n" +
-                        bodyData +
-                        "\r\n</Body>===========================");
-
-                // Charset 확인용 print
-                System.out.println("Charset.defaultCharset(): " + Charset.defaultCharset());
-
-                OutputStream outputStream = socket.getOutputStream();
-                String msg = "";
-                msg += "<link rel='icon' href='data:,'/>\r\n";  //favicon.ico 를 로드하지 않게 설정
-                msg += "<script type='text/javascript'>\r\n";
-                msg += "    function console_print(){\r\n";
-                msg += "        var id = document.getElementById('Id').value;\r\n";
-                msg += "        console.log(id);\r\n";
-                msg += "    };\r\n";
-                msg += "</script>\r\n";
-                msg += "<body>\r\n";
-                msg += "    <input id='Id' type='text'/>\r\n";
-                msg += "    <a id='click' href='#' onclick='console_print()'>click</a></br></br>\r\n";
-                msg += "    <form method='POST' action='/'>\r\n";
-                msg += "        <input type='text' name='sampleIdPost'/>\r\n";
-                msg += "        <input type='password' name='samplePwdPost'/>\r\n";
-                msg += "        <button>POST_sumbit</button>\r\n";
-                msg += "    </form>\r\n";
-                msg += "    <form method='GET' action='/'>\r\n";
-                msg += "        <input type='text' name='sampleIdGet'/>\r\n";
-                msg += "        <input type='text' name='samplePwdGet'/>\r\n";
-                msg += "        <button>GET_sumbit</button>\r\n";
-                msg += "    </form>\r\n";
-                msg += "    <form method='POST' action='/' enctype='multipart/form-data'>\r\n";
-                msg += "        <input type='file' name='sampleFile'/>\r\n";
-                msg += "        <button>File_sumbit</button>\r\n";
-                msg += "    </form>\r\n";
-                msg += "</body>\r\n";
-
-                outputStream.write(new String("HTTP/1.1 200 OK\r\n").getBytes());
-                outputStream.write(new String("Content-Length:" + msg.getBytes().length + "\r\n").getBytes());
-                outputStream.write(new String("Content-Type:text/html,charset=UTF-8\r\n\r\n").getBytes());
-                outputStream.write(msg.getBytes());
-
-                //화면 출력
-                outputStream.flush();
-
-                inputStream.close();
-                outputStream.close();
-                socket.close();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -152,11 +220,10 @@ public class Main {
     }
 
     /**
-     *
-     * @param values - Body에서 날라온 데이터를 해석한 String들
-     * @param map - 해석한 String의 데이터를 삽입해줄 Map
+     * @param values Body에서 날라온 데이터를 해석한 String들
+     * @param map    해석한 String의 데이터를 삽입해줄 Map
      */
-    public static void parameterPut(String[] values, Map map){
+    public static void parameterPut(String[] values, Map map) {
         for (int i = 0; i < values.length; i++) {
             String[] getParameter = values[i].split("=");
             if (getParameter.length > 1) {
