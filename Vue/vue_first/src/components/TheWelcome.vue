38577940719175f4cb31548f4d89f0323237f850
ABCD <yoondongsub92@gmail.com>
2023-07-27 09:00:03 +0900

    Vue RenderFunctions Study

--- Vue/vue_first/src/components/TheWelcome.vue
+++ Vue/vue_first/src/components/TheWelcome.vue
@@ -6,12 +6,14 @@ import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
 import Props from "@/components/HowToUseProps.vue";
-import { ref, reactive } from 'vue';
+import { h, ref, reactive } from 'vue';
 import HowToUseProxy from "@/components/HowToUseProxy.vue";
 import HowToUseLoopIs from "@/components/HowToUseLoopIs.vue";
+import HowToUseRenderFunctions from "@/components/HowToUseRenderFunctions.vue"
 
 export default {
     components: {
+        HowToUseRenderFunctions,
         HowToUseProxy,
         WelcomeItem,
         DocumentationIcon,
@@ -29,9 +31,13 @@ export default {
             {id : '0', naming : '이씨'},
             {id : '0', naming : '하씨'},
         ])
+
+        h('div', { id: 'foo'}, 'Reset')
+
         return {
             list,
-            a: '//www.naver.com'
+            a: '//www.naver.com',
+            h
         }
     }
 }
@@ -39,82 +45,6 @@ export default {
 </script>
 
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
-
   <WelcomeItem>
       <!--<Props :user-name="list"></Props>-->
       <how-to-use-proxy/>
@@ -124,4 +54,5 @@ export default {
   <a :href.prop="a">왓더</a>
   <a :href.attr="'www.daum.net'" :href="a" >나나</a>
   <a :href="a" :href.prop="'www.daum.net'" >나나</a>
+  <how-to-use-render-functions/>
 </template>
