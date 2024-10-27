615442e81704ce4fc8bdd47699a7c0f62af1ed99
ABCD <ABCtoDev@gmail.com>
2024-10-27 14:53:10 +0900

    모션센서 setting

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,11 +1,23 @@
 #include "Arduino.h"
 
-int a = 30;
+int val = 0;
 
 void setup() {
   // 프로그램 시작시 최초 1번 실행
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // 프로그램 후 반복적으로 실행
+  val = digitalRead(2);
+
+  if (val == HIGH) {
+    digitalWrite(3, HIGH);
+  } else {
+    digitalWrite(3, LOW);
+  }
+
+  Serial.println(val);
 }
