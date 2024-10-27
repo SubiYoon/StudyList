c8b21154d8196633574bcd08e994c9eeb17fb391
ABCD <ABCtoDev@gmail.com>
2024-10-27 16:09:00 +0900

    점점 밝아지는 led

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,20 +1,9 @@
 #include "Arduino.h"
 
-int val = 0;
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(8, OUTPUT);
-}
+void setup() { pinMode(3, OUTPUT); }
 void loop() {
-  val = analogRead(A0);
-  if (val < 200) {
-    Serial.println("ON");
-    digitalWrite(8, HIGH);
-  } else {
-    Serial.println("OFF");
-    digitalWrite(8, LOW);
+  for (int i = 0; i < 255; i += 10) {
+    analogWrite(3, i);
+    delay(100);
   }
-  Serial.println(val);
-  delay(100);
 }
