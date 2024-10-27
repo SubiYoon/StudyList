530a8c07209fb5142827c9b5894754c77bd6d464
ABCD <ABCtoDev@gmail.com>
2024-10-27 15:51:29 +0900

    조도센서를 이용한 LED turn on/off

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,8 +1,20 @@
 #include "Arduino.h"
-int analog = 0;
-void setup() { Serial.begin(9600); }
+
+int val = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(8, OUTPUT);
+}
 void loop() {
-  analog = analogRead(A0);
-  Serial.println(analog);
-  delay(1000);
+  val = analogRead(A0);
+  if (val < 200) {
+    Serial.println("ON");
+    digitalWrite(8, HIGH);
+  } else {
+    Serial.println("OFF");
+    digitalWrite(8, LOW);
+  }
+  Serial.println(val);
+  delay(100);
 }
