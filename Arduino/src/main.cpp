864669b525cbdacfa7dcad206765c8f04efbc764
ABCD <ABCtoDev@gmail.com>
2024-10-29 20:27:31 +0900

    arduino init

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -2,10 +2,5 @@
 #include "LiquidCrystal.h"
 #include "Wire.h"
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
-void setup() {
-  lcd.begin(16, 2);
-  lcd.print("I'm stil hungry...");
-}
+void setup() {}
 void loop() {}
