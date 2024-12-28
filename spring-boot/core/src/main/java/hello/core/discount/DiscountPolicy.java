9093ad5cf63f90d52dc8c75b03bc9e0a8adfb332
ABCD <ABCtoDev@gmail.com>
2024-12-28 20:13:11 +0900

    spring study 03

---
+++ spring-boot/core/src/main/java/hello/core/discount/DiscountPolicy.java
@@ -0,0 +1,14 @@
+package hello.core.discount;
+
+import hello.core.member.Member;
+import org.springframework.core.annotation.Order;
+
+public interface DiscountPolicy {
+    /**
+     *
+     * @param member 유저
+     * @param price 가격
+     * @return 할인 대상 금액
+     */
+    int discount(Member member, int price);
+}
