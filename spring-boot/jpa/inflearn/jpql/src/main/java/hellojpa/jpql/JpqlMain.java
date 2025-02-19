ce7c6036c9590b700c4e47851a6232ed54d0d3c5
ABCD <ABCtoDev@gmail.com>
2025-02-19 23:13:33 +0900

    spring boot study jpa 08(join fetch)

--- spring-boot/jpa/inflearn/jpql/src/main/java/hellojpa/jpql/JpqlMain.java
+++ spring-boot/jpa/inflearn/jpql/src/main/java/hellojpa/jpql/JpqlMain.java
@@ -1,6 +1,7 @@
 package hellojpa.jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
@@ -15,35 +16,53 @@ public class JpqlMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            Team team1 = new Team();
+            team1.setName("팀A");
+            Team team2 = new Team();
+            team2.setName("팀B");
+            Team team3 = new Team();
+            team3.setName("팀C");
 
-            String query1 = em.createQuery("select m.username from Member m where m.username = :membername", String.class)
-                    .setParameter("membername", "member1")
-                    .getSingleResult();
-            TypedQuery<Member> query2 = em.createQuery("select m from Member m", Member.class);
-            List<MemberDTO> query3 = em.createQuery("select new hellojpa.jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class).getResultList();
+            em.persist(team1);
+            em.persist(team2);
+            em.persist(team3);
 
-            System.out.println("resultList = " + query3.get(0).toString());
-            System.out.println("singleResult = " + query1);
-
-            System.out.println("===============================================");
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(team1);
+            em.persist(member1);
 
             Member member2 = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            member.setMemberType(MemberType.USER);
+            member2.setUsername("회원2");
+            member2.setTeam(team2);
             em.persist(member2);
 
-            String query = "select m.username, 'Hello', true from Member m where m.memberType = hellojpa.jpql.MemberType.USER";
-            List<Object[]> result = em.createQuery(query).getResultList();
+            Member member3 = new Member();
+            member3.setUsername("회원1");
+            member3.setTeam(team3);
+            em.persist(member3);
 
-            for (Object[] objects : result) {
-                System.out.println("objects[0] = " + objects[0]);
-                System.out.println("objects[1] = " + objects[1]);
-                System.out.println("objects[2] = " + objects[2]);
+            Member member4 = new Member();
+            member4.setUsername("회원4");
+            member4.setTeam(team1);
+            em.persist(member4);
+
+            em.flush();
+            em.clear();
+
+            String query1 = "select m from Member m join fetch m.team";
+            List<Member> result1 = em.createQuery(query1, Member.class).getResultList();
+
+            System.out.println("result = " + result1);
+
+            // distinct를 사용하면 결과 3개, 사용안하면 결과가 4개 - SQL쿼리의 distinct랑 같다고 생각하면 안됨
+            // Team에 대한 중복값을 DB에서가 아닌 애플리케이션에서 JPA가 중복값을 제거해줌
+            // 1 : N 조인시 Data가 뻥튀기 되는 현상이 있기 때문
+            String query2 = "select t from Team t join fetch t.members";
+            List<Team> result2 = em.createQuery(query2, Team.class).getResultList();
+
+            for (Team team : result2) {
+                System.out.println(team);
             }
 
             tx.commit();
